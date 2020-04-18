40d95219c5e41b3dd01b0e515133911b99df6aa3
Joshua May <notjosh@gmail.com>
2020-04-18 17:49:42 +0200

    Breakin' tests

--- src/__tests__/decode.ts
+++ src/__tests__/decode.ts
@@ -2,7 +2,7 @@ import decode from '../decode';
 
 describe('decode', () => {
   it('works with example', () => {
-    const claimCode = '6xwh-441j-8115-zyrh';
+    const claimCode = '6xwh-441j-8115-zyrhx';
     const expectedKey = 'F7D9bmztHV32+WJScGZR0g==';
 
     const result = decode(claimCode);
