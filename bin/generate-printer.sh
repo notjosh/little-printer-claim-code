02622ea6850305cdc704a21ee9d33c4179f6dfc8
Joshua May <notjosh@gmail.com>
2020-05-16 13:56:40 +0200

    Fixed command line generator

--- bin/generate-printer.sh
+++ bin/generate-printer.sh
@@ -4,8 +4,10 @@
 [ ! -d dist ] && yarn dist
 node -e " \
 const encode = require('./dist/encode').default; \
-const address = '$(openssl rand -hex 8)'; \
-const secret = Math.floor(Math.random() * 0xffffffffff); \
-const info = { address, secret, 'claim code': encode(parseInt(address, 16) & 0xffffff, secret) }; \
+const hardwareXorFromDeviceAddress = require('./dist/bitshuffle').hardwareXorFromDeviceAddress; \
+const address = BigInt(\`0x$(openssl rand -hex 8)\`); \
+const secret = BigInt(\`0x$(openssl rand -hex 5)\`); \
+const xor = hardwareXorFromDeviceAddress(address); \
+const info = { address: address.toString(16), secret: secret.toString(16), xor: xor.toString(10), 'claim code': encode(xor, secret) }; \
 Object.keys(info).forEach((key) => { console.log(\`\${key.padStart(12, ' ')}: \${info[key]}\`); }); \
 "
